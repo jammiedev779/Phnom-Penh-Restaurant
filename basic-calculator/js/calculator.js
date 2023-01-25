91147b5ee7a64188df97b7212ac012226fc7f53b
Jammie-Dev779 <vinunsombath@gmail.com>
2023-01-25 23:24:30 +0700

    1.3

--- basic-calculator/js/calculator.js
+++ basic-calculator/js/calculator.js
@@ -1,10 +1,16 @@
+class Calculate {
+
+}
+
 const numbers_class = [".num0", ".num1",".num2",".num3",".num4",".num5",".num6",".num7",".num8",".num9"]
 const operations_class = [".add",".minus",".multiply",".divide"]
-const configs_class = [".config1",".config2",".dot",".config3",".config4",".equal"]
+const configs_class = [".history",".CE",".C",".dot",".equal"]
 
-let input = document.querySelector(".input")
-let inputString = []
-let buffer = ""
+let curr_number = ""
+let prev_number = ""
+let curr_input = document.querySelector(".curr-input")
+let prev_input = document.querySelector(".prev-input")
+let operator
 //number buttons
 const numbers = []
 for(let i=0 ; i<numbers_class.length ; i++){
@@ -13,13 +19,7 @@ for(let i=0 ; i<numbers_class.length ; i++){
 for(let i=0 ; i<numbers_class.length ; i++){
     numbers[i] = numbers_class[i].addEventListener("click",function(){
         onclick(numbers_class[i])
-        buffer += numbers_class[i].innerHTML
-        console.log(buffer)
-        
-        document.querySelector(".operation").classList.remove("ristricted")
     })
-    inputString[inputString.length] = buffer
-    console.log("input " + inputString)
 }
 
 
@@ -30,78 +30,70 @@ for(let i=0 ; i<operations_class.length ; i++){
 }
 for(let i=0 ; i<operations_class.length ; i++){
     operations[i] = operations_class[i].addEventListener("click",function(){
-        if(!document.querySelector(".operation").classList.contains("ristricted")){
-            document.querySelector(".operation").classList.add("ristricted")
-            document.querySelector(".dot").classList.remove("ristricted")
-            onclick(operations_class[i])
-            
-        }
+        prev_number = curr_input.innerHTML
+        onclick(operations_class[i])
+        operator = i
+        document.querySelector("iconify-icon").style.fontSize = "1rem"
+        prev_input.innerHTML = curr_input.innerHTML
+        curr_input.innerHTML = "0"
     })
 }
 
-// "(",")","CE","X",".","=" button
+// "CE","X",".","=" button
 const configs = []
 for(let i=0 ; i<configs_class.length ; i++){
     configs_class[i] = document.querySelector(configs_class[i])
 }
 
-// for "(" & ")" function
-for(let i=0 ; i<2 ; i++){
-    configs[i] = configs_class[i].addEventListener("click",function(){
-        if(i == 0){
-            if(document.querySelector(".operation").classList.contains("ristricted") || input.innerHTML.length == 0){
-                if(!document.querySelector(".config1").classList.contains("ristricted")){
-                    document.querySelector(".config1").classList.add("ristricted")
-                    onclick(configs_class[i])
-                }
-            }
-        }
-        else{
-            if(document.querySelector(".config1").classList.contains("ristricted")){
-                let detectBranket = input.innerHTML.split("(")
-                if(detectBranket[1].length > 0 && !document.querySelector(".operation").classList.contains("ristricted")){
-                    //document.querySelector(".config2").classList.add("ristricted")
-                    document.querySelector(".config1").classList.remove("ristricted")
-                    onclick(configs_class[i])
-                }
-            }
-        }
-    })
-}
-
 //for "." function
-configs_class[2].addEventListener("click",function(){
+configs_class[3].addEventListener("click",function(){
     if(!document.querySelector(".dot").classList.contains("ristricted")){
         document.querySelector(".dot").classList.add("ristricted")
-        onclick(configs_class[2])
+        onclick(configs_class[3])
     }
 })
 
-//for "CE" function
-configs_class[3].addEventListener("click",function(){
-    input.innerHTML = "0"
-    document.querySelector(".dot").classList.remove("ristricted")
-    document.querySelector(".config1").classList.remove("ristricted")
-    document.querySelector(".operation").classList.remove("ristricted")
+//for "CE" & "C" function
+for(let i=1 ; i<=2 ; i++){
+    configs_class[i].addEventListener("click",function(){
+        curr_input.innerHTML = "0"
+        prev_input.innerHTML = ""
+        document.querySelector(".dot").classList.remove("ristricted")
+    })
+}
+
+// "=" function
+configs_class[4].addEventListener("click",()=> {
+    curr_number = curr_input.innerHTML
+
+    if(operator == 0) curr_input.innerHTML = add()
+    else if(operator == 1) curr_input.innerHTML = subtract()
+    else if(operator == 2) curr_input.innerHTML = multiply()
+    else curr_input.innerHTML = divide()
+    prev_input.innerHTML = ""
+    console.log("=")
 })
 
-let arr = []
-let a = "a"
-let b = "b"
-arr.push(a)
-arr.pop()
-arr.push(b)
-console.log(arr)
-
-
 function onclick(element){
-        if(input.innerHTML == "0"){
-            input.innerHTML = element.innerHTML
+        if(curr_input.innerHTML == "0" && element.innerHTML != "."){
+            curr_input.innerHTML = element.innerHTML
         }
-        else if(input.innerHTML.length != 15){
-            input.innerHTML = input.innerHTML.concat(element.innerHTML)
-           // console.log(input.innerHTML.length)
+        else if(curr_input.innerHTML.length != 15){
+            curr_input.innerHTML += element.innerHTML
         }
+}
+
+function add(){
+    return Number(prev_number) + Number(curr_number)
+}
+function subtract(){
+    return Number(prev_number) - Number(curr_number)
+}
+function multiply(){
+    return Number(prev_number) * Number(curr_number)
+}
+function divide(){
+    return Number(prev_number) / Number(curr_number)
 }
 
 
